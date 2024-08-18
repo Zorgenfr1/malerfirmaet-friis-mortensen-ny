fdb488a46ee85aa7250b9b536de36ae5f0a17854
Zorgenfr1 <frodesfmortensen@gmail.com>
2024-08-18 15:11:41 +0200

    added javascript

---
+++ script.js
@@ -0,0 +1,19 @@
+function SendMail() {
+    var params = {
+        from_name : document.getElementById("fullName").value,
+        email_id : document.getElementById("email-id").value,
+        message : document.getElementById("message").value
+    };
+
+    emailjs.send("service_hpfv0sh", "template_tfi1bby", params).then(function (res) {
+        alert("Besked Sendt!" + res.status);
+
+        document.getElementById("fullName").value = "";
+        document.getElementById("email-id").value = "";
+        document.getElementById("message").value = "";
+    }).catch(function (error) {
+        console.error("Failed to send email:", error);
+        alert("Der opstod en fejl ved afsendelse. Prøv igen senere.");
+    });
+    
+};
